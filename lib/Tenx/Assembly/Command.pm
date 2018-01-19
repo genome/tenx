6dc35ead6bbe94abba4c1f503294d5f16a525761
Eddie Belter <ebelter@gmail.com>
2018-01-18 21:57:39 -0600

    Fix assembly target name

--- lib/Tenx/Assembly/Command.pm
+++ lib/Tenx/Assembly/Command.pm
@@ -6,7 +6,7 @@ use warnings 'FATAL';
 use UR::Object::Command::Crud;
 UR::Object::Command::Crud->create_command_subclasses(
     target_class => 'Tenx::Assembly',
-    target_name => 'alignment',
+    target_name => 'assembly',
     namespace => 'Tenx::Assembly::Command',
     sub_command_configs => {
         copy => { skip => 1, },
